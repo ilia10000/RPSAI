675eb030f1072f826f16b82da275873b472bfe91
ilia10000 <s.ilia@rogers.com>
2016-02-13 13:34:16 -0500

    First commit - Core logic

---
+++ RPSAI.py
@@ -0,0 +1,97 @@
+from random import choice
+
+choices = list("RPS")
+
+print choices
+cwins=0
+pwins=0
+beats = {"R":"P","P":"S","S":"R"}
+
+
+def makeMove(inputs,outputs,history):
+    move = ""
+    length = len(inputs)
+    start=length
+    if length>15:
+        start=15
+    
+    for k in range(length-start,length-1):
+        if length==0:
+            return choice(choices)
+        ins = "".join(inputs[k:])
+        if ins in history[:-1]:
+            rcount=0
+            pcount=0
+            scount=0
+            prev=-1
+            for i in range(history.count(ins)):
+                prev1=history.find(ins,prev+1)
+                if prev1+len(ins) <= len(history)-1:
+                    next1 = history[prev1 + len(ins)]
+                
+                
+                     
+                if next1 =="R":
+                    rcount+=1
+                elif next1=="P":
+                    pcount+=1
+                else:
+                    scount+=1
+                prev = prev1
+            if max(rcount,pcount,scount)==rcount:
+                move = "R"
+            elif max(rcount,pcount,scount)==pcount:
+                move="P"
+            elif max(rcount,pcount,scount)==scount:
+                move = "S"
+            '''print ins
+            print "next is: " + move
+            print history.count(ins)
+            print "r" + str(rcount)
+            print "p" + str(pcount)
+            print "s" + str(scount)'''
+            
+            return beats[move]
+    return choice(choices)
+
+
+def RPS():
+    myfile = open("F.txt","w+")
+    history = myfile.read()
+    myfile.seek(0,2)
+    cwins=0
+    pwins=0
+    inputs=[]
+    outputs=[]
+    print("Choose 'R', 'P' or 'S' to make a move, choose 'E' to exit.")
+    while True:
+        choice1 =raw_input()
+        
+        if choice1!="":
+            choice1=choice1.upper()
+        if choice1 == "E":
+            
+            break
+        elif choice1 in list("RPS"):
+            
+            move=makeMove(inputs,outputs,history)
+            if beats[move]==choice1:
+                pwins+=1
+            elif beats[choice1]==move:
+                cwins+=1
+            print (choice1 + "  vs.  " + move)
+            
+            print str(pwins) + " " + str(cwins) 
+            if len(inputs)>1:
+                history+=inputs[-2]
+            inputs.append(choice1)
+            outputs.append(move)
+        else:
+            print "Invalid move"
+    
+    myfile.close()    
+    return ("Player: " + str(pwins) + "  Cpu: " +str(cwins))
+
+            
+        
+print RPS()
