b05b5c20eb85b4500e17b1f9e725ef308d41eb9f
ilia10000 <s.ilia@rogers.com>
2016-06-07 18:25:29 -0700

    Vastly fleshing out meta

--- RPSAI.py
+++ RPSAI.py
@@ -1,12 +1,34 @@
 from random import choice
 form numpy import prod
+import time
 choices = list("RPS")
 window = 15
 print choices
 cwins=0
 pwins=0
 beats = {"R":"P","P":"S","S":"R"}
+
+#####################################################################################
+#################################### ALGORITHMS #####################################
+#####################################################################################
+def w_algo1(w_inputs,w_parameters):
+    history = w_inputs[0]
+    ins=w_inputs[1]
+    input=[history.count(ins),len(ins)]
+    return prod([i[0]**i[1] for i in zip(input,w_parameters)])
+
+def w_algo2(w_inputs,w_parameters):
+    history = w_inputs[0]
+    ins=w_inputs[1]
+    return (history.count(ins)**w_parameters[0])/(1.0*(len(ins)**w_parameters[1]))
+
 w_algos = [w_algo1, w_algo2]
+#####################################################################################
+#####################################################################################
+#####################################################################################
+
+
+
 
 #Scans for current pattern in history to predict next player move
 def makeMove(inputs,outputs,history,w_algo, w_parameters):
@@ -68,19 +90,18 @@ def gen_rand():
 
 
 #Runs rock-paper-scissors games until player exits, returns the result.
-import time
 
-def RPS(meta=False, name="", to_load="", algo=1, timestr=""):
+def RPS(meta=False, name="", to_load="", algo=1, timestr="", moves_in="", w_parameters=""):
     if not meta:
         timestr = time.strftime("%Y%m%d-%H%M%S")
         name = raw_input("Name: ")
         to_load = raw_input("Filename: ")
         algo = raw_input("Algo: ")
-        history=""
-        if len(to_load)>0:
-            preload = open(to_load, "a+") #File to load data from
-            history = preload.read() #Load data
-            preload.close()
+    history=""
+    if len(to_load)>0:
+        preload = open(to_load, "a+") #File to load data from
+        history = preload.read() #Load data
+        preload.close()
     filename = name+"_"+timestr
     metafile = filename+ "_meta"
     myfile = open(filename,"a+") #File to write to
@@ -94,9 +115,14 @@ def RPS(meta=False, name="", to_load="", algo=1, timestr=""):
     counter=0
     print("Choose 'R', 'P' or 'S' to make a move, I for summary statistics, choose 'E' to exit.")
     #Main game loop
+
     while True:
         e_flag=False
-        choices =raw_input() #Read input
+        if moves_in=="":
+            choices =raw_input() #Read input
+        else:
+            choices = moves_in
+            moves_in =""
         for choice1 in choices:
             if choice1!="": #Avoids error with empty input
                 choice1=choice1.upper() #Standardize input to caps
@@ -130,22 +156,50 @@ def RPS(meta=False, name="", to_load="", algo=1, timestr=""):
     return [pwins, cwins, ties, total]
 
 
-def w_algo1(w_inputs,w_parameters):
-    history = w_inputs[0]
-    ins=w_inputs[1]
-    input=[history.count(ins),len(ins)]
-    return prod([i[0]**i[1] for i in zip(input,w_parameters)])
-def w_algo2(w_inputs,w_parameters):
-    history = w_inputs[0]
-    ins=w_inputs[1]
-    return (history.count(ins)**w_parameters[0])/(1.0*(len(ins)**w_parameters[1]))
+def param_gradient(n=10, w_algo2use =1, config_w_parameters =[[0,5],[0,5]], moves_in="", name="", to_load="", timestr="" ):
+    param_names =[]
+    best_params=[]
+    best_score = 0
+    for i in range(len(config_w_parameters)):
+        best_params.append(config_w_parameters[i][0])
+        param_names.append("param{0}".format(i))
+    param_names = ",".join(param_names)
+    to_run = ""
+    for i in range(len(config_w_parameters)):
+        to_run+="\t"*i
+        to_run+= "for {0} in range({1},{2}):\n".format(param_names[i],config_w_parameters[i][0], config_w_parameters[i][1])
+    to_run+= "\t"*(i+1)
+    to_run+="results = RPS(True,{0}, {1}, {2}, {3}, {4}, [{5}])\n".format(name, to_load, w_algo2use, timestr, moves_in, param_names)
+    to_run+= "\t"*(i+1)
+    to_run+="score = (1.0*results[1])/(1.0*results[3])\n"
+    for j in range(n-1):
+        to_run+="results = RPS(True,{0}, {1}, {2}, {3}, {4}, [{5}])\n".format(name, to_load, w_algo2use, timestr, moves_in, param_names)
+        to_run+= "\t"*(i+1)
+        to_run+="score += (1.0*results[1])/(1.0*results[3])\n"
+        to_run+= "\t"*(i+1)
+    to_run+="score = score/{0}".format(n)
+    to_run+="if score > best_score:\n"
+    to_run+= "\t"*(i+2)
+    to_run+="best_score = score\n"
+    to_run+= "\t"*(i+2)
+    to_run+="best_params = [{0}]\n".format(param_names)
+    return [best_params, best_score]
+
+def meta(n=10, w_algos2use = [1,2] , config_w_parameters = [[[0,5],[0,5]],[[0,5],[0,5]]], moves_in="" ):
+    accuracies = {}
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    name = raw_input("Name: ")
+    to_load = raw_input("Filename: ")
+    for i in range(len(w_algos2use)):
+        w_algo2use = w_algos2use[i]
+        accuracies[str(w_algo2use)] = param_gradient(n, w_algo2use,config_w_parameters[i],moves_in,name,to_load, timestr)
+
+    return accuracies
 
 
-def meta(n=10, iterations=10, w_algos = [1,2] , n_w_parameters = [2,2] ):
 
 
 
 
-
-        
-print RPS()
+moves_in="RPS"
+print meta(moves_in)
